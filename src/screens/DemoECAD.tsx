d64865b76ff6d72c29a0a167558de9fcc898a34f
Matheus Guimarães <matheusguim13@gmail.com>
2023-07-21 16:38:44 -0300

    Histórico de relatórios adicionado

--- src/screens/DemoECAD.tsx
+++ src/screens/DemoECAD.tsx
@@ -1,4 +1,4 @@
-import { Box, Text, VStack } from "native-base";
+import { Box, FlatList, Text, VStack, View } from "native-base";
 import LogoSocinpro from '@assets/logos/logosemnome.png'
 import { Image } from "native-base";
 import { ScreenHeader } from "@components/ScreenHeader";
@@ -11,6 +11,16 @@ import LogoECAD from '@assets/logos/logo-ecad-home.svg'
 import MonthPicker from "react-native-month-year-picker";
 
 export function DemoECAD(){
+
+
+  const [relatoriosGerados, setRelatoriosGerados] = useState(
+    [
+      dayjs().format('MM/YY'), dayjs('03/2023').format('MM/YY')
+    ]
+  )
+
+
+
 
   const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
   const [selectedDate, setSelectedDate] = useState<dayjs.Dayjs>(dayjs());
@@ -72,14 +82,15 @@ export function DemoECAD(){
           <Box
           backgroundColor={"gray.600"}
           width={"full"}
-          height={'80%'}
+          height={'40%'}
           borderRadius={8}
           >
               
               <VStack
-              justifyContent={'space-evenly'}
+              justifyContent={'flex-end'}
               alignItems={'center'}
               paddingX={10}
+              marginTop={15}
               >
                 <Text
                   color={'gray.100'}
@@ -90,6 +101,23 @@ export function DemoECAD(){
                   Selecione o mês
                 </Text>
                 
+                <TouchableOpacity
+              onPress={()=>setDatePickerVisibility(true)}
+              style={{
+                backgroundColor:'#15b94c',
+                borderRadius:6
+    
+              }}
+              >
+                
+                <Text
+                color={'gray.100'}
+                backgroundColor={'green.600'}
+                fontSize={'lg'}
+                textAlign={'center'}
+                width={32}
+                >{selectedDate.format('MM/YY')}</Text>
+              </TouchableOpacity>
   
                 
                 {isDatePickerVisible && (
@@ -101,10 +129,83 @@ export function DemoECAD(){
                     locale="pt" // Definindo o idioma (opcional)
                   />
                 )}
-              
+              <TouchableOpacity
+              style={{
+                backgroundColor:'#00875F',
+                width:150,
+                height:35,
+                borderRadius:10,
+                justifyContent:'center',
+                alignItems:'center',
+                marginTop:20
+              }}
+              >
+                <Text
+                color={'white'}
+                >Gerar Relatório
+                </Text>
+              </TouchableOpacity>
                 
               </VStack>
               
+          </Box>
+
+          <Box
+          backgroundColor={'gray.600'}
+          width={'100%'}
+          height={'40%'}
+          marginTop={5}
+          borderRadius={10}
+          
+          >
+            <VStack
+            alignItems={'center'}
+            >
+              <Text
+              color={'gray.100'}
+              textAlign={'center'}
+              
+              >Últimos relatórios gerados</Text>
+
+              <FlatList
+              data={relatoriosGerados}
+              ItemSeparatorComponent={()=> (
+              <View
+                h={0.5}
+                w={'full'}
+                backgroundColor={'gray.300'}
+                />
+                )
+              }
+              keyExtractor={item => item}
+              renderItem={({item})=> (
+
+                 
+                <TouchableOpacity
+              onPress={()=>setDatePickerVisibility(true)}
+              style={{
+                backgroundColor:'#15b94c',
+                borderRadius:6
+    
+              }}
+              >
+                
+                <Text
+                color={'gray.100'}
+                backgroundColor={'green.600'}
+                fontSize={'lg'}
+                textAlign={'center'}
+                width={32}
+                >{item}</Text>
+              </TouchableOpacity>
+              )
+            
+            }
+              
+              />
+
+            </VStack>
+
           </Box>
         
        </VStack>
